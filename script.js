55a0089888074dac6c29e1af9e4f2627baba6be0
Jerry Bi <j39bi@edu.uwaterloo.ca>
2021-01-01 12:29:31 -0600

    Fixed navbar, changed footer. Site now shareable

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("mobileNav");
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+    } else { 
+      x.className = x.className.replace(" w3-show", "");
+    }
+  }
